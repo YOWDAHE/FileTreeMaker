a4cb974a3b4c660d365e9dff7a2c527ca1506358
Yodahe Ketema <ketemayodahe@gmail.com>
2023-09-09 13:14:45 +0300

    attempting the tree maker

--- filetree/app/j/file.js
+++ filetree/app/j/file.js
@@ -1,41 +1,51 @@
-// Import the file system module
 const fs = require('fs');
 
-// Import the path module
-const p = require('path');
+p = require('path');
 
-// Define a function that takes a path to a file and performs a dfs
-function dfs(path, pre) {
-    // Check if the path exists
+
+const data = {};
+
+export default function dfs(path, pre, currentNode = data) {
+
     if (fs.existsSync(path)) {
-        // Get the stats of the path
+
         let stats = fs.statSync(path);
-        // If the path is a directory
+
         if (stats.isDirectory()) {
-            // Print the name of the directory without the extension
-            console.log(pre ,p.basename(path));
-            // Get the list of files and subdirectories in the directory
+
+            console.log(pre, p.basename(path));
+            console.log('currentNode', currentNode)
+
+            currentNode['name'] = p.basename(path);
+            currentNode.children = []
+
             let files = fs.readdirSync(path);
-            // Loop through each file or subdirectory
+
             for (let file of files) {
-                // Recursively call dfs on the file or subdirectory with the full path
-                dfs(path + '/' + file, pre + '.');
+
+                let childNode = {};
+                
+                currentNode.children.push(childNode);
+                
+                dfs(path + '/' + file, pre + '.', childNode);
             }
         }
-        // If the path is a file
         else if (stats.isFile()) {
-            // Get the file name with the extension
+
             let file = p.basename(path);
-            // Print only the file name
+
             console.log(file);
+            // Set the name and children properties of the current node
+            currentNode['name'] = file;
+            currentNode.children = [];
         }
     }
-    // If the path does not exist
     else {
-        // Throw an error
+
         throw new Error('Invalid path');
     }
+    
+    return data
 }
 
-// Call dfs on a sample path
-dfs('C:/Users/ketem/Documents/Code/projects/TreeMaker/FileTreeMaker/filetree/app', '');
+dfs('C:/Users/ketem/Documents/Code/projects/TreeMaker/FileTreeMaker/filetree/app', '', data);
